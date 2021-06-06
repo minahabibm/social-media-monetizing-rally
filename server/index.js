b781517ebf85df46c2bf35c85abb3e3f543ce41e
Mina <mhcsc01@gmail.com>
2021-06-06 00:34:27 -0400

    nicely fixed the bug (fixing a bug with safari streaming server (refreshing the page after the first time you start the server, and the client to play the videos)

--- server/index.js
+++ server/index.js
@@ -29,104 +29,131 @@ app.get('/download/', (req, res) => {
 });
 
 app.get('/stream-safari/', (req, res) => {
-
-    // Handle File Download to Stream.
-    try { 
-        const encodedUrl = req.query.url;
-        const decodedUrl = atob(encodedUrl);
-        let pathname = new URL(decodedUrl).pathname;
-        let pathnameArr = pathname.split('/');
-        var fileName = pathnameArr[pathnameArr.length - 1].replace('%', '_');
-        var filePathDir = folderName + '/' + fileName
-        if (!fs.existsSync(filePathDir)) {
-            const filePath = fs.createWriteStream(filePathDir);
-            axios.get(decodedUrl, {responseType: 'stream'}).then(response => response.data.pipe(filePath)).catch(err => console.log("err"));
+    // Handle File Download For Stream.
+    let getFilePromise = new Promise(function(resolve, reject) {
+        try { 
+            const encodedUrl = req.query.url;
+            const decodedUrl = atob(encodedUrl);
+            let pathname = new URL(decodedUrl).pathname;
+            let pathnameArr = pathname.split('/');
+            var fileName = pathnameArr[pathnameArr.length - 1].replace('%', '_');
+            var filePathDir = folderName + '/' + fileName
+            if (!fs.existsSync(filePathDir)) {
+                const filePath = fs.createWriteStream(filePathDir);
+                axios
+                    .get(decodedUrl, {responseType: 'stream'})
+                    .then(response => {
+                        response.data.pipe(filePath);
+                        let error = null;
+                        filePath.on('error', err => {
+                            error = err;
+                            writer.close();
+                            reject(err);
+                        });
+                        filePath.on('close', () => {
+                            if (!error) {
+                              resolve([fileName, filePathDir]);
+                            }
+                        })
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        reject(err);
+                        
+                    });
+            }else {
+                resolve([fileName, filePathDir]);
+            }
+        } catch (err) {
+            console.log(err);
+            reject(err);
+           
         }
-    } catch (err) {
-        console.log(err)
-    }
+    });
 
-    console.log("Stream for Video " + fileName);
     // Stream the Video
-    try {
-        const options = {};
-        let start;
-        let end;
-
-        const range = req.headers.range;
-        if (range) {
-            const bytesPrefix = "bytes=";
-            if (range.startsWith(bytesPrefix)) {
-                const bytesRange = range.substring(bytesPrefix.length);
-                const parts = bytesRange.split("-");
-                if (parts.length === 2) {
-                    const rangeStart = parts[0] && parts[0].trim();
-                    if (rangeStart && rangeStart.length > 0) {
-                        options.start = start = parseInt(rangeStart);
+    getFilePromise.then(([fileName, filePathDir]) => {
+        console.log("Stream for Video " + fileName);
+        try {
+            const options = {};
+            let start;
+            let end;
+    
+            const range = req.headers.range;
+            if (range) {
+                const bytesPrefix = "bytes=";
+                if (range.startsWith(bytesPrefix)) {
+                    const bytesRange = range.substring(bytesPrefix.length);
+                    const parts = bytesRange.split("-");
+                    if (parts.length === 2) {
+                        const rangeStart = parts[0] && parts[0].trim();
+                        if (rangeStart && rangeStart.length > 0) {
+                            options.start = start = parseInt(rangeStart);
+                        }
+                        const rangeEnd = parts[1] && parts[1].trim();
+                        if (rangeEnd && rangeEnd.length > 0) {
+                            options.end = end = parseInt(rangeEnd);
+                        }
                     }
-                    const rangeEnd = parts[1] && parts[1].trim();
-                    if (rangeEnd && rangeEnd.length > 0) {
-                        options.end = end = parseInt(rangeEnd);
-                    }
                 }
             }
-        }
-
-        res.setHeader("content-type", "video/mp4");
-        fs.stat(filePathDir, (err, stat) => {
-            if (err) {
-                console.error(`File stat error for ${filePathDir}.`);
-                console.error(err);
-                res.sendStatus(500);
-                return;
-            }
-
-            let contentLength = stat.size;
     
-            if (req.method === "HEAD") {
-                res.statusCode = 200;
-                res.setHeader("accept-ranges", "bytes");
-                res.setHeader("content-length", contentLength);
-                res.end();
-            }
-            else {       
-                let retrievedLength;
-                if (start !== undefined && end !== undefined) {
-                    retrievedLength = (end+1) - start;
-                }
-                else if (start !== undefined) {
-                    retrievedLength = contentLength - start;
-                }
-                else if (end !== undefined) {
-                    retrievedLength = (end+1);
-                }
-                else {
-                    retrievedLength = contentLength;
-                }
-    
-                res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
-    
-                res.setHeader("content-length", retrievedLength);
-    
-                if (range !== undefined) {  
-                    res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
-                    res.setHeader("accept-ranges", "bytes");
-                }
-    
-                // Listing 7.
-                const fileStream = fs.createReadStream(filePathDir, options);
-                fileStream.on("error", error => {
-                    console.log(`Error reading file ${filePathDir}.`);
-                    console.log(error);
+            res.setHeader("content-type", "video/mp4");
+            fs.stat(filePathDir, (err, stat) => {
+                if (err) {
+                    console.error(`File stat error for ${filePathDir}.`);
+                    console.error(err);
                     res.sendStatus(500);
-                });
+                    return;
+                }
     
-                fileStream.pipe(res);
-            }
-        })
-    } catch(err) {
-        console.log(err)
-    }
+                let contentLength = stat.size;
+        
+                if (req.method === "HEAD") {
+                    res.statusCode = 200;
+                    res.setHeader("accept-ranges", "bytes");
+                    res.setHeader("content-length", contentLength);
+                    res.end();
+                }
+                else {       
+                    let retrievedLength;
+                    if (start !== undefined && end !== undefined) {
+                        retrievedLength = (end+1) - start;
+                    }
+                    else if (start !== undefined) {
+                        retrievedLength = contentLength - start;
+                    }
+                    else if (end !== undefined) {
+                        retrievedLength = (end+1);
+                    }
+                    else {
+                        retrievedLength = contentLength;
+                    }
+        
+                    res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+        
+                    res.setHeader("content-length", retrievedLength);
+        
+                    if (range !== undefined) {  
+                        res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
+                        res.setHeader("accept-ranges", "bytes");
+                    }
+        
+                    // Listing 7.
+                    const fileStream = fs.createReadStream(filePathDir, options);
+                    fileStream.on("error", error => {
+                        console.log(`Error reading file ${filePathDir}.`);
+                        console.log(error);
+                        res.sendStatus(500);
+                    });
+        
+                    fileStream.pipe(res);
+                }
+            })
+        } catch(err) {
+            console.log(err)
+        }
+    })
 
 });
 
